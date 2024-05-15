ceab5baacd0c3fbd5923aeb7c6836bb60ff753f8
carlosDAM2905 <carlosdam2905@gmail.com>
2024-05-15 18:10:53 +0200

    terminado el ejercicio sumar

--- src/main/java/com/aulafx/prueba1/controlador/SumarController.java
+++ src/main/java/com/aulafx/prueba1/controlador/SumarController.java
@@ -1,14 +1,33 @@
 package com.aulafx.prueba1.controlador;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SumarController {
+
+    // declarar los elementos de la vista que van a jugar un papelen la aplicación
     @FXML
-    private Label welcomeText;
+    private Label lblResultado;
+    @FXML
+    private TextField txtOperador1;
+    @FXML
+    private TextField txtOperador2;
+    @FXML
+    private Button btnSumar;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void sumar(ActionEvent actionEvent) {
+        int operador1 = Integer.parseInt(txtOperador1.getText());
+
+        int operador2 = Integer.parseInt(txtOperador2.getText());
+
+        int suma = operador1 + operador2;
+        // quiero que esta suma la muestre la etiqueta resultado
+
+        lblResultado.setText(String.valueOf(suma));
     }
 }
